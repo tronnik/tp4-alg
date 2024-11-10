769d34085795128949b8ed2b7d6f90f8b1d603d6
Francisco <franjonas98@hotmail.com>
2024-11-10 17:18:55 -0300

    add tow more 3d fugures

--- tp4/main.cpp
+++ tp4/main.cpp
@@ -5,103 +5,124 @@
 
 
 void DrawWireframe(const std::array<raylib::Vector3, 8>& Mesh,
-                   const bool DrawVertices = false) {
-  //rightside
-  Mesh[0].DrawLine3D(Mesh[1], raylib::Color::White());
-  Mesh[1].DrawLine3D(Mesh[2], raylib::Color::White());
-  Mesh[2].DrawLine3D(Mesh[3], raylib::Color::White());
-  Mesh[3].DrawLine3D(Mesh[0], raylib::Color::White());
+	const bool DrawVertices = false) {
+	//rightside
+	Mesh[0].DrawLine3D(Mesh[1], raylib::Color::White());
+	Mesh[1].DrawLine3D(Mesh[2], raylib::Color::White());
+	Mesh[2].DrawLine3D(Mesh[3], raylib::Color::White());
+	Mesh[3].DrawLine3D(Mesh[0], raylib::Color::White());
 
-  //leftside
-  Mesh[4].DrawLine3D(Mesh[5], raylib::Color::White());
-  Mesh[5].DrawLine3D(Mesh[6], raylib::Color::White());
-  Mesh[6].DrawLine3D(Mesh[7], raylib::Color::White());
-  Mesh[7].DrawLine3D(Mesh[4], raylib::Color::White());
+	//leftside
+	Mesh[4].DrawLine3D(Mesh[5], raylib::Color::White());
+	Mesh[5].DrawLine3D(Mesh[6], raylib::Color::White());
+	Mesh[6].DrawLine3D(Mesh[7], raylib::Color::White());
+	Mesh[7].DrawLine3D(Mesh[4], raylib::Color::White());
 
-  //Front
-  Mesh[3].DrawLine3D(Mesh[7], raylib::Color::White());
-  Mesh[4].DrawLine3D(Mesh[0], raylib::Color::White());
+	//Front
+	Mesh[3].DrawLine3D(Mesh[7], raylib::Color::White());
+	Mesh[4].DrawLine3D(Mesh[0], raylib::Color::White());
 
-  //back
-  Mesh[2].DrawLine3D(Mesh[6], raylib::Color::White());
-  Mesh[5].DrawLine3D(Mesh[1], raylib::Color::White());
+	//back
+	Mesh[2].DrawLine3D(Mesh[6], raylib::Color::White());
+	Mesh[5].DrawLine3D(Mesh[1], raylib::Color::White());
 
-  if (DrawVertices) {
-    Mesh[0].DrawSphere(0.5, RED);
-    Mesh[1].DrawSphere(0.5, RED);
-    Mesh[2].DrawSphere(0.5, RED);
-    Mesh[3].DrawSphere(0.5, RED);
-    Mesh[4].DrawSphere(0.5, RED);
-    Mesh[5].DrawSphere(0.5, RED);
-    Mesh[6].DrawSphere(0.5, RED);
-    Mesh[7].DrawSphere(0.5, RED);
-  }
+	if (DrawVertices) {
+		Mesh[0].DrawSphere(0.5, RED);
+		Mesh[1].DrawSphere(0.5, RED);
+		Mesh[2].DrawSphere(0.5, RED);
+		Mesh[3].DrawSphere(0.5, RED);
+		Mesh[4].DrawSphere(0.5, RED);
+		Mesh[5].DrawSphere(0.5, RED);
+		Mesh[6].DrawSphere(0.5, RED);
+		Mesh[7].DrawSphere(0.5, RED);
+	}
 }
 
 
 
 int main(void) {
-  raylib::Window Window;
-  raylib::Camera RealCam;
-  raylib::Camera FakeCam;
-  Window.Init();
+	raylib::Window Window;
+	raylib::Camera RealCam;
+	raylib::Camera FakeCam;
+	Window.Init();
 
-  // Define the camera to look into our 3d world
-  RealCam.position = {10.0f, 10.0f, 10.0f};
-  RealCam.target = {0.0f, 0.0f, 0.0f};
-  RealCam.up = {0.0f, 1.0f, 0.0f};
-  RealCam.fovy = 45.0f;
-  RealCam.projection = CAMERA_PERSPECTIVE;
+	// Define the camera to look into our 3d world
+	RealCam.position = { 10.0f, 10.0f, 10.0f };
+	RealCam.target = { 0.0f, 0.0f, 0.0f };
+	RealCam.up = { 0.0f, 1.0f, 0.0f };
+	RealCam.fovy = 45.0f;
+	RealCam.projection = CAMERA_PERSPECTIVE;
 
-  FakeCam.position = {10.0f, 10.0f, 10.0f};
-  FakeCam.target = {0.0f, 0.0f, 0.0f};
-  FakeCam.up = {0.0f, 1.0f, 0.0f};
-  FakeCam.fovy = 45.0f;
-  FakeCam.projection = CAMERA_PERSPECTIVE;
+	FakeCam.position = { 10.0f, 10.0f, 10.0f };
+	FakeCam.target = { 0.0f, 0.0f, 0.0f };
+	FakeCam.up = { 0.0f, 1.0f, 0.0f };
+	FakeCam.fovy = 45.0f;
+	FakeCam.projection = CAMERA_PERSPECTIVE;
 
-  Frustum Frustum(20, FakeCam);
+	Frustum Frustum(20, FakeCam);
 
-  DisableCursor();
+	DisableCursor();
 
-  Window.SetTargetFPS(60);
+	Window.SetTargetFPS(60);
 
-  raylib::Model Knot = LoadModelFromMesh(GenMeshKnot(1.0f, 2.0f, 16, 128));
+	raylib::Model Knot = LoadModelFromMesh(GenMeshKnot(1.0f, 2.0f, 16, 128));
+	raylib::Model Cube = LoadModelFromMesh(GenMeshCube(2.0f, 1.0f, 2.0f));
+	raylib::Model Sphere = LoadModelFromMesh(GenMeshSphere(2, 32, 32));
 
-  auto Origin = raylib::Vector3(0, 0, 0);
+	auto Origin = raylib::Vector3(0, 0, 0);
+	auto Origin2 = raylib::Vector3(5, 0, 5);
+	auto Origin3 = raylib::Vector3(-5, 0, -10);
 
-  auto KnotAABB = AABB(Knot.GetMeshes()->vertices,
-                       Knot.GetMeshes()->vertexCount, Origin);
+	auto KnotAABB = AABB(Knot.GetMeshes()->vertices,
+		Knot.GetMeshes()->vertexCount, Origin);
 
-  // Main game loop
-  while (!WindowShouldClose()) {
-    RealCam.Update(CAMERA_FREE);
+	auto CubeAABB = AABB(Cube.GetMeshes()->vertices,
+		Cube.GetMeshes()->vertexCount, Origin2);
 
-    Frustum.Update(FakeCam);
+	auto SphereAABB = AABB(Sphere.GetMeshes()->vertices,
+		Sphere.GetMeshes()->vertexCount, Origin3);
 
-    Frustum.UpdatePlanes();
+	// Main game loop
+	while (!WindowShouldClose())
+	{
+		RealCam.Update(CAMERA_FREE);
 
-    Window.BeginDrawing();
+		Frustum.Update(RealCam);
 
-    RealCam.BeginMode();
+		Frustum.UpdatePlanes();
 
-    Window.ClearBackground(BLACK);
+		Window.BeginDrawing();
 
-    DrawWireframe(Frustum.GetMesh());
+		RealCam.BeginMode();
 
-    DrawWireframe(KnotAABB.GetMesh());
+		Window.ClearBackground(BLACK);
 
-    if (Frustum.IsObjectVisible(KnotAABB.GetMesh())) {
-      DrawModel(Knot, Origin, 1.0f, RED);
-    }
+		DrawWireframe(Frustum.GetMesh());
 
-    DrawGrid(20, 1);
+		DrawWireframe(KnotAABB.GetMesh());
+		DrawWireframe(CubeAABB.GetMesh());
+		DrawWireframe(SphereAABB.GetMesh());
 
-    RealCam.EndMode();
+		if (Frustum.IsObjectVisible(KnotAABB.GetMesh()))
+		{
+			DrawModel(Knot, Origin, 1.0f, RED);
+		}
+		if (Frustum.IsObjectVisible(CubeAABB.GetMesh()))
+		{
+			DrawModel(Cube, Origin2, 1.0f, RED);
+		}
+		if (Frustum.IsObjectVisible(SphereAABB.GetMesh()))
+		{
+			DrawModel(Sphere, Origin3, 1.0f, RED);
+		}
+		DrawGrid(20, 1);
 
-    Window.EndDrawing();
-  }
+		RealCam.EndMode();
 
-  Window.Close(); // Close window and OpenGL context
+		Window.EndDrawing();
+	}
 
-  return 0;
+	Window.Close(); // Close window and OpenGL context
+
+	return 0;
 }
